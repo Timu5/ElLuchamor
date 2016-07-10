723feee59965ab9fb8469175dc0ebe39466d644e
timu5 <timusziomek@gmail.com>
2016-07-11 01:32:32 +0200

    Renderer Class

---
+++ SDLSharp/Renderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SDL2;
+
+namespace SDLSharp
+{
+	public class Renderer
+	{
+		static IntPtr renderer;
+		public Renderer(IntPtr _renderer)
+		{
+			renderer = _renderer;
+		}
+
+		public static void DrawLine(Vector2 start, Vector2 end)
+		{
+			DrawLine ((int)start.X, (int)start.Y, (int)end.X, (int)end.Y);
+		}
+
+		public static void DrawLine(int x1, int y1, int x2, int y2)
+		{
+			SDL.SDL_RenderDrawLine (renderer, x1, y1, x2, y2);
+		}
+	}
+}
+
