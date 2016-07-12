6ef88da118dca6d5032f3a6ee3b1ac208ea073f9
timuss <timusziomek@gmail.com>
2016-07-12 23:15:12 +0200

    Add Math Class

---
+++ SDLSharp/MathE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDLSharp
+{
+    public static class MathE
+    {
+        public static float Lerp(float f, float v1, float t)
+        {
+            return (1 - t) * f + t * v1;
+        }
+    }
+}
